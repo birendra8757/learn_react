c8748d686d32a50e2eb3912ec64f1845c10e8f11
birendra8757 <computersinghaniya@gmail.com>
2024-02-01 00:05:27 +0530

    project setup

---
+++ blog-project/src/config/config.js
@@ -0,0 +1,11 @@
+const config ={
+appWriteUrl: String(import.meta.env.VITE_APP_WRITE_URL),
+appWriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+appWriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
+appWriteCollectionId: String(import.meta.env.VITE_COLLATION_ID),
+appWriteBucketId: String(import.meta.env.VITE_BUCKET_ID),
+
+}
+
+
+export default config
